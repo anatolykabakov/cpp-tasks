5d6ef437e24ed4e126c455bc92cc23a783e77275
Anatoly Kabakov <akabakov@stdev.ru>
2021-11-17 19:30:22 +0300

    ADD: solve 5 task.

--- chapter1/5.cpp
+++ chapter1/5.cpp
@@ -1,5 +1,5 @@
 /*
-Task 5: TODO.
+Task 5: Write the program, that calculates the binomial coefficient. Check user input. .
 */
 #include <iostream>
 
@@ -11,16 +11,23 @@ const int factorial(const int& number) {
 }
 
 const int binominal(const int& n, const int& k) {
-    if (n <= k) {
-        return 0;
-    }
     return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+bool is_lower_binominal_coefficient_more_than_zero(const int& lower_binominal_coefficient) {
+    if (lower_binominal_coefficient <= 0) {
+        return false;
+    }
+    return true;
 }
 
 int main() {
     int n = 0;
     std::cin >> n;
-    for (int k = 0; k < n; k++) { 
+    if (!is_lower_binominal_coefficient_more_than_zero(n)){
+        return EXIT_FAILURE;
+    }
+    for (int k = 0; k <= n; k++) {
         std::cout << binominal(n, k) << std::endl;
     }
     return EXIT_SUCCESS;
