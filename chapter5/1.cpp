d779f839dcb4c9dd4e869ddcf583c3a2f6a3ec00
Anatoly Kabakov <akabakov@stdev.ru>
2022-02-10 00:11:10 +0300

    NEW:  task 1 in progress, added operator -=, which decresing mass, needed to add description and code for solving task

--- chapter5/1.cpp
+++ chapter5/1.cpp
@@ -1,42 +1,68 @@
 #include <iostream>
 
-struct ParalelepipedParams {
-  const double width{0};
-  const double height{0};
-  const double depth{0};
-
-  void show() {
-    std::cout << "Width: " << width << std::endl;
-    std::cout << "Height: " << height << std::endl;
-    std::cout << "Depth: " << depth << std::endl;
-  }
-};
-
-// struct ParalelepipedExtendedParams : public ParalelepipedParams
-// {
-//   const double mass{0};
-//   ParalelepipedExtendedParams(
-//     const ParalelepipedParams &params,
-//     const double mass): 
-//     ParalelepipedParams(params.) {}
-// };
-
-
 class Parralelepiped {
 public:
-  Parralelepiped(const ParalelepipedParams &params):
-  params_(params) {}
-  double get_volume() const {
-    return params_.width * params_.height * params_.depth;
+  Parralelepiped(
+    const double width,
+    const double height,
+    const double depth):
+  width_(width),
+  height_(height),
+  depth_(depth) {
+    std::cout << __func__ << std::endl;
   }
+  double get_volume() const {
+    return width_ * height_ * depth_;
+  }
+
+  void show_parameters() {
+    std::cout << "Width is " << width_ << std::endl;
+    std::cout << "Height is " << height_ << std::endl;
+    std::cout << "Depth is " << depth_ << std::endl;
+  }
+  
 private:
-  ParalelepipedParams params_;
+  double width_{0};
+  double height_{0};
+  double depth_{0};
+};
+
+class ParalelepipedExtended : public Parralelepiped {
+public:
+  ParalelepipedExtended(
+    const double width,
+    const double height,
+    const double depth,
+    const double mass): 
+    Parralelepiped(width, height, depth),
+    mass_(mass) {
+      std::cout << __func__ << std::endl;
+    }
+    void show_mass() const {
+      std::cout << mass_ << std::endl;
+    }
+
+    double get_density() const {
+      return mass_ / get_volume();
+    }
+
+    ParalelepipedExtended& operator -=(const double mass) {
+      mass_ -= mass;
+    }
+private:
+  double mass_{0};
 };
 
 int main() {
-  ParalelepipedParams params{10,20,30};
-  params.show();
-  Parralelepiped parralelepiped(params);
+  Parralelepiped parralelepiped(10, 20, 30);
+  parralelepiped.show_parameters();
   std::cout << "Volume is " << parralelepiped.get_volume() << std::endl;
+
+  ParalelepipedExtended paralelepiped_extended(40, 50, 60, 70);
+  paralelepiped_extended.show_parameters();
+  std::cout << "Density is " << paralelepiped_extended.get_density() << std::endl;
+
+  paralelepiped_extended -= 2.0;
+  paralelepiped_extended.show_mass();
   return 0;
 }
