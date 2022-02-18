6a8862e0d083d12a945f53cd8483ebf4a6d0467c
Anatoly Kabakov <akabakov@stdev.ru>
2022-02-18 08:34:24 +0300

    NEW: Description for task 4 is added.

---
+++ chapter5/ 4.cpp
@@ -0,0 +1,25 @@
+/*
+Chapter 5, task 4.
+Description: Write the program with class for describe vehicle.
+Class vehicle has parameters: model, color, volume of fuel storage in litres.
+Provide method allow calculate the distance which drive the vehicle, 
+using up a full fuel storage (Calculates as multiply on 100 relation volume 
+fuel storage to the norm consumption gasoline). 
+
+Based on the Vehicle class, create derived class the truck vehicle.
+Addind parameter, describe carrying capacity (The max weight of cargo, 
+that can be carried the vehicle). and the method that allow calculate 
+cost price of tranportation the one tonn of cargo on one kilometr.
+
+The result of method is calculate as: norm consumption gasoline devided on 100,
+multiply on fuel price, and devided on carrying capacity. 
+The price of fuel pass by argument into method.
+
+Consider using operator methods and funcs. 
+*/
+
+#include <iostream>
+
+int main() {
+  return 0;
+}
