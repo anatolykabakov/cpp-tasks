e39cbd0f87b6809c3885cf7017b8a0277092f418
Anatoly Kabakov <akabakov@stdev.ru>
2022-02-23 22:54:50 +0300

    Add: some doxygen docs is added

--- include/chapter5/task1.h
+++ include/chapter5/task1.h
@@ -1,4 +1,6 @@
 /**
+ * @date 2022-02-23
+ * @author Anatoly Kabakov <anatoly.kabakov@mail.ru>
  * @brief Chapter 5, task 1.
  * @details Write the program, which creates the class to describe a physical
  * object the parallelepiped. Class has three parameters: width, depth and height,
@@ -12,21 +14,51 @@
  */
 #pragma once
 #include <iostream>
-
+/**
+ * @brief Class discribe paralelepiped
+ *
+ */
 class Parralelepiped {
  public:
+  /**
+   * @brief Construct a new Parralelepiped object
+   *
+   * @param width Width of paralelepiped. [meters]
+   * @param height Height of paralelepiped. [meters]
+   * @param depth Depth of paralelepiped. [meters]
+   */
   Parralelepiped(const double width, const double height, const double depth)
       : width_(width), height_(height), depth_(depth) {
     std::cout << __func__ << std::endl;
   }
+  /**
+   * @brief Get the volume of paralelepiped.
+   *
+   * @return Volume of paralelepiped.
+   */
   double get_volume() const { return width_ * height_ * depth_; }
-
+  /**
+   * @brief Get the width of paralelepiped.
+   *
+   * @return Width of paralelepiped. [meters]
+   */
   double get_width() const { return width_; }
-
+  /**
+   * @brief Get the height of paralelepiped.
+   *
+   * @return Height of paralelepiped. [meters]
+   */
   double get_height() const { return height_; }
-
+  /**
+   * @brief Get the depth of paralelepiped.
+   *
+   * @return Depth of paralelepiped. [meters]
+   */
   double get_depth() const { return depth_; }
-
+  /**
+   * @brief This method show parameters of paralelepiped in console.
+   *
+   */
   void show_parameters() {
     std::cout << "Width is " << width_ << std::endl;
     std::cout << "Height is " << height_ << std::endl;
@@ -34,8 +66,17 @@ class Parralelepiped {
   }
 
  protected:
+  /**
+   * @brief Width of paralellelepiped. [meters]
+   */
   double width_{0};
+  /**
+   * @brief Height of paralellelepiped. [meters]
+   */
   double height_{0};
+  /**
+   * @brief Depth of paralellelepiped. [meters]
+   */
   double depth_{0};
 };
 
